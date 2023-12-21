190c5da36c8e4c5b146343eec176a07987293bc4
Michaelk324 <147104243+Michaelk324@users.noreply.github.com>
2023-12-21 22:12:51 +0000

    innerHTML

--- domManipulation.js
+++ domManipulation.js
@@ -13,6 +13,7 @@ let image = document.querySelector('.image');
 // CODE ALONG
 // 1. An event listener is provided for the `contentButton`
 // 2. When clicked, it should change the content of:
+
 //   - The `name` header.
 
 // -- RETURN TO LESSON --
@@ -22,22 +23,20 @@ let image = document.querySelector('.image');
 //   - The `location` paragraph.
 
 contentButton.addEventListener('click', function() {
-
-
-
-
-
-
+  myName.innerHTML = "";
+  myHobby.innerHTML = "";
+  myLocation.innerHTML = "";
 });
 
 // 3. Add an event listener to the `image1Button`
+image1Button.addEventListener('click', function() {
 // 4. When clicked, it should change the source of `image` to another image URL.
-
-
-
-
-
+  image.src = "https://media0.giphy.com/media/GA1sltMVrl4YJgMyh5/giphy.gif";
+});
 // 5. Add an event listener to the `image2Button`
+image2Button.addEventListener('click', function() {
+  image.src = "https://i.pinimg.com/originals/0f/7c/b3/0f7cb3ca1ce88fe28e5be48f202dd45d.gif";
+});
 // 6. When clicked, it should change the source of `image` to another image URL.
 
 
